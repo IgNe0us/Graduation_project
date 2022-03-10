9abf8d764d19c51ac03276d6e656c19ec19e1bc6
IgNe0us <wjdals1831@naver.com>
2022-03-10 22:24:39 +0900

    운영 날짜, 타이머, 게이지 구현
    운영 날짜, 타이머, 게이지 구현

---
+++ Demo01/Assets/Scripts/Enemies/EnemyMove.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+
+public class EnemyMove : MonoBehaviour
+{
+    public Rigidbody2D rigid;
+    public int nextMove;
+    float nextThinkTime;
+    Animator anim;
+    SpriteRenderer spriteRenderer;
+
+    //애니메이션에 대한 Enum
+
+
+    Player player;
+
+    //HP 관련
+    public int hp = 50;
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        nextThinkTime = Random.Range(2f, 6f);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        Think();
+        
+        
+    }
+
+    private void Update()
+    {
+        Die();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //move
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+
+        //Platform Check
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove * 0.3f, rigid.position.y );
+        Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
+        if (rayHit.collider == null)
+        {
+            Turn();
+        }
+
+    }
+
+    void Think()
+    {
+        anim.SetInteger("WalkSpeed", nextMove);
+        nextMove = Random.Range(-1, 2);
+        Invoke("Think", nextThinkTime);
+    }
+
+    void Turn()
+    {
+        nextMove = nextMove * -1;
+        spriteRenderer.flipX = nextMove == 1;
+
+
+        CancelInvoke();
+        Invoke("Think", nextThinkTime);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        hp -= damage;
+    }
+
+
+
+    private void Die()
+    {
+        if(hp <= 0)
+        {
+            player = GameObject.Find("Player").GetComponent<Player>();
+            player.money += Random.Range(30, 50) ;
+            ItemDatabase.instance.ItemDrop(rigid.position, 24);
+            Destroy(gameObject);
+        }
+    }
+
+
+}
---
+++ Demo01/Assets/Scripts/Items/ItemHealing.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenuAttribute(menuName = "ItemEft/Consumable/Health")]
+public class ItemHealing : ItemEffect
+{
+    Player player;
+    public override bool ExecuteRole()
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();
+        player.curHp += 10;
+        return true;
+    }
+}
