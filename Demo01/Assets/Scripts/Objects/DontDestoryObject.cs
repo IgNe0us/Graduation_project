581ead48c94f51e361f02eea06a10c1b2f29c011
IgNe0us <wjdals1831@naver.com>
2022-03-22 23:50:34 +0900

    dontdestroy 버그수정
    dontdestroy 버그수정

--- Demo01/Assets/Scripts/Objects/DontDestoryObject.cs
+++ Demo01/Assets/Scripts/Objects/DontDestoryObject.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DontDestoryObject : MonoBehaviour
 {
+    public string transferMapName;
     private void Awake()
     {
-        DontDestroyOnLoad(gameObject);
+        var obj = FindObjectsOfType<DontDestoryObject>();
+
+        if (obj.Length == 5) // 파괴되지 않을 오브젝트 수의 개수
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
 }
